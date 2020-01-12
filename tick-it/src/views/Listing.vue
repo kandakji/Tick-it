f1a78922150ee9c14796ca4c91a07a39937e01d6
pmath2012 <pmath2012@live.com>
2020-01-12 19:41:38 +0100

    Adding images to go with the descriptions

--- tick-it/src/views/Listing.vue
+++ tick-it/src/views/Listing.vue
@@ -5,34 +5,44 @@
             <b-tabs  content-class="mt-3" align="center">
                 <b-tab title="On Sale" :class="{ active: onSaleTab}">
                     <b-card no-body v-for="sItem in filteredOnSale" :key="sItem.id">
+					<div class="card-tab">
+						<b-img-lazy class="card-img" :src="buildIconUrl(sItem.icon)"> </b-img-lazy>
                         <b-card-body :title="sItem.title"  :sub-title="sItem.artist">
+							<div class="card-holder">
                             <div class="card-info">
                                 <b-card-text>
                                     <p>At {{ sItem.venue }}</p>
-                                    <p>On {{ sItem.date }} - {{sItem.city}},{{sItem.country}}</p>
+                                    <p>On {{ sItem.date }} - {{sItem.city}},{{sItem.country}}</p>
                                 </b-card-text>
                                 <div class="card-btn">
                                     <b-button v-bind:href="'/onsale/'+ sItem.id" variant="primary">Buy Now</b-button>
                                     <b-button v-bind:href="'/onsale/'+ sItem.id" variant="primary">Details</b-button>
                                 </div>
                             </div>
+							</div>
                         </b-card-body>
+					</div>
                     </b-card>
                 </b-tab>
                 <b-tab title="Requested">
                     <b-card no-body v-for="rItem in filteredRequested" :key="rItem.id">
+					<div class="card-tab">
+						<b-img-lazy class="card-img" :src="buildIconUrl(rItem.icon)"> </b-img-lazy>
                         <b-card-body :title="rItem.title"  :sub-title="rItem.artist">
-                            <div class="card-info">
+                            <div class="card-holder">
+							<div class="card-info">
                                 <b-card-text>
                                     <p>At {{ rItem.venue }}</p>
-                                    <p>On {{ rItem.date }} - {{rItem.city}},{{rItem.country}}</p>
+                                    <p>On {{ rItem.date }} - {{rItem.city}},{{rItem.country}}</p>
                                 </b-card-text>
                                 <div class="card-btn">
                                     <b-button v-bind:href="'/request/'+ rItem.id" variant="primary">Make Offer</b-button>
                                     <b-button v-bind:href="'/request/'+ rItem.id" variant="primary">Details</b-button>
                                 </div>
                             </div>
+							</div>
                         </b-card-body>
+						</div>
                     </b-card>
                 </b-tab>
             </b-tabs>
@@ -46,9 +56,7 @@
     import onSaleEvents from '../assets/onSale.json'
     import requestedEvents from '../assets/requested.json'
     export default {
-
         name: 'listing',
-
         data() {
             return {
                 search: '',
@@ -75,78 +83,76 @@
                 this.search = to.query.search;
             }     
         },
-
-
+		methods: {
+			buildIconUrl(icon) {
+				return require(`../assets/${icon}`);
+			}
+		}
     }
-
 </script>
 
 
 
 <style>
-
 .card{
     width: 100%;
     display: table;
     margin: 1%;
 }
-
 .card-body{
-    width: 100%;
+	display:table-cell;
+    width: 80%;
 }
-
-.card{
-    padding:1%;
-    width: 100%;
-}
-
 a {
     color: #17a2b8;
 }
-
 a:hover {
   color: #17a2b8;
 }
-
 .tabs{
     margin-top: 2%;
 }
-
 .card-title{
     text-align: left;
     width: 100%;
 }
-
 .card-subtitle {
     text-align: left;
 }
-
 .card-info{
     display: table-row;
+	width: 80%;
 }
-
 .card-text{
     text-align: left;
     display: table-cell;
-    width: 90%;
+    width: 70%;
 }
-
 .card-btn {
     display: table-cell;
-    width: 10%;
+    width: 30%;
 }
-
+.card-tab{
+	display: table-row;
+}
+.card-img{
+	display: table-cell;
+	witdth: 20vw;
+	height: 20vw;
+	vertical-align: top;
+}
+.card-holder{
+	display: table;
+	width: 100%;
+	
+}
 .btn-primary {
     margin: 1%;
     width: 90%;
     background-color: #17a2b8;
     border-color: #ffffff;
 }
-
 .btn-primary:hover{
     background-color: #14b5ce;
 }
-
-
-
 </style>
